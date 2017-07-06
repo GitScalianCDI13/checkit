2800947c4a34f101980230ed2e025a6f39c16178
fred <fremaitre@gmail.com>
2017-07-06 10:31:30 +0200

    Mapping

--- business/src/main/java/com/scalian/checkit/service/mapping/ThemeMapping.java
+++ business/src/main/java/com/scalian/checkit/service/mapping/ThemeMapping.java
@@ -1,4 +1,29 @@
 package com.scalian.checkit.service.mapping;
 
+import com.scalian.checkit.model.ThemeEntity;
+import com.scalian.checkit.service.model.ThemeBO;
+
 public class ThemeMapping {
+
+    public static ThemeBO mapThemeEntityToBO(ThemeEntity themeEntity){
+        ThemeBO themeBO = new ThemeBO();
+        themeBO.setThemeId(themeEntity.getThemeId());
+        themeBO.setThemeLabel(themeEntity.getThemeLabel());
+        //List<QuestionBO> questions;
+        //List<TestBO> tests;
+
+
+
+        return themeBO;
+    }
+
+    public static ThemeEntity mapThemeBOToEntity(ThemeBO themeBO){
+        ThemeEntity themeEntity = new ThemeEntity();
+        themeEntity.setThemeId(themeBO.getThemeId());
+        themeEntity.setThemeLabel(themeBO.getThemeLabel());
+        //List<QuestionBO> questions;
+        //List<TestBO> tests;
+
+        return themeEntity;
+    }
 }
--- business/src/main/java/com/scalian/checkit/service/mapping/UserResponseMapping.java
+++ business/src/main/java/com/scalian/checkit/service/mapping/UserResponseMapping.java
@@ -1,4 +1,16 @@
 package com.scalian.checkit.service.mapping;
 
+import com.scalian.checkit.model.UserResponseEntity;
+import com.scalian.checkit.service.model.UserResponseBO;
+
 public class UserResponseMapping {
+
+    public static UserResponseBO mapUserResponseEntityToBO(UserResponseEntity UserResponseEntity){
+        UserResponseBO userResponseBO = new UserResponseBO();
+        return userResponseBO;
+    }
+    public static UserResponseEntity mapUserResponseBOToEntity(UserResponseBO userResponseBO){
+        UserResponseEntity userResponseEntity = new UserResponseEntity();
+        return userResponseEntity;
+    }
 }
