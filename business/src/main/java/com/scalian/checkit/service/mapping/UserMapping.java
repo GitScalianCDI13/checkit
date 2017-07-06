cd38249a88632cdfe35167f377795254d3c39546
fred <fremaitre@gmail.com>
2017-07-06 13:03:34 +0200

    Mapping

--- business/src/main/java/com/scalian/checkit/service/mapping/UserMapping.java
+++ business/src/main/java/com/scalian/checkit/service/mapping/UserMapping.java
@@ -1,10 +1,17 @@
 package com.scalian.checkit.service.mapping;
 
+import com.scalian.checkit.model.ResultEvaluationEntity;
 import com.scalian.checkit.model.UserEntity;
+import com.scalian.checkit.model.UserResponseEntity;
+import com.scalian.checkit.service.model.ResultEvaluationBO;
 import com.scalian.checkit.service.model.UserBO;
+import com.scalian.checkit.service.model.UserResponseBO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserMapping {
-    public static UserBO mapUserEntityToBO(UserEntity userEntity){
+    public static UserBO mapUserEntityToBO(UserEntity userEntity) {
         UserBO userBO = new UserBO();
         userBO.setUserId(userEntity.getUserId());
         userBO.setRole(RoleMapping.mapRoleEntityToBO(userEntity.getRole()));
@@ -15,10 +22,30 @@ public class UserMapping {
         userBO.setUserExperiency(userEntity.getUserExperiency());
         userBO.setUserProjects(userEntity.getUserProjects());
         userBO.setUserPassword(userEntity.getUserPassword());
+
+        // Map des QuestionEntityToBO
+        List<ResultEvaluationBO> resultEvaluationBOList = new ArrayList<>();
+        for (ResultEvaluationEntity resultEvaluationEntity : userEntity.getResultEvaluations()) {
+            ResultEvaluationBO resultEvaluationBO = ResultEvaluationMapping.mapResultEvaluationEntityToBO(resultEvaluationEntity);
+            resultEvaluationBOList.add(resultEvaluationBO);
+        }
+        userBO.setResultEvaluations(resultEvaluationBOList);
+
+
+        // Map des UserResponseToBO
+
+        List<UserResponseBO> userResponseBOList = new ArrayList<>();
+        for (UserResponseEntity userResponseEntity : userEntity.getUserResponses()) {
+            UserResponseBO userResponseBO = UserResponseMapping.mapUserResponseEntityToBO(userResponseEntity);
+            userResponseBOList.add(userResponseBO);
+        }
+        userBO.setUserResponses(userResponseBOList);
+
         return userBO;
     }
 
-    public static UserEntity mapUserBOToEntity(UserBO userBO){
+
+    public static UserEntity mapUserBOToEntity(UserBO userBO) {
         UserEntity userEntity = new UserEntity();
         userEntity.setUserId(userBO.getUserId());
         userEntity.setRole(RoleMapping.mapRoleBOToEntity(userBO.getRole()));
@@ -29,6 +56,28 @@ public class UserMapping {
         userEntity.setUserExperiency(userBO.getUserExperiency());
         userEntity.setUserProjects(userBO.getUserProjects());
         userEntity.setUserPassword(userBO.getUserPassword());
+
+
+        // Map des QuestionBOToEntity
+        List<ResultEvaluationEntity> resultEvaluationEntityList = new ArrayList<>();
+        for (ResultEvaluationBO resultEvaluationBO : userBO.getResultEvaluations()) {
+            ResultEvaluationEntity resultEvaluationEntity = ResultEvaluationMapping.mapEvaluationBOToEntity(resultEvaluationBO);
+            resultEvaluationEntityList.add(resultEvaluationEntity);
+        }
+        userEntity.setResultEvaluations(resultEvaluationEntityList);
+
+
+        // Map des UserResponseBOToEntity
+
+        List<UserResponseEntity> userResponseEntityList = new ArrayList<>();
+        for (UserResponseBO userResponseBO : userBO.getUserResponses()) {
+            UserResponseEntity userResponseEntity = UserResponseMapping.mapUserResponseBOToEntity(userResponseBO);
+            userResponseEntityList.add(userResponseEntity);
+        }
+        userEntity.setUserResponses(userResponseEntityList);
+
         return userEntity;
     }
+
+
 }
