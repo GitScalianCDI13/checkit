f1c8a1e422db8de49ccc94e3774322394378a8a6
fred <fremaitre@gmail.com>
2017-07-06 09:43:49 +0200

    BO Classes

--- business/src/main/java/com/scalian/checkit/service/model/TestBO.java
+++ business/src/main/java/com/scalian/checkit/service/model/TestBO.java
@@ -1,10 +1,19 @@
 package com.scalian.checkit.service.model;
 
+import java.util.List;
+
 public class TestBO {
 
     private Integer testId;
     private String testLabel;
     private Integer themeId;
+    private String testImage;
+    private String testDescription;
+    private String testSynopsis;
+    private List<EvaluationBO> evaluations;
+    private List<QuestionBO> questions;
+    private ThemeBO theme;
+    private List<TestResultBO> testResults;
 
     public Integer getTestId() {
         return testId;
@@ -12,6 +21,62 @@ public class TestBO {
 
     public void setTestId(Integer testId) {
         this.testId = testId;
+    }
+
+    public String getTestImage() {
+        return testImage;
+    }
+
+    public void setTestImage(String testImage) {
+        this.testImage = testImage;
+    }
+
+    public String getTestDescription() {
+        return testDescription;
+    }
+
+    public void setTestDescription(String testDescription) {
+        this.testDescription = testDescription;
+    }
+
+    public String getTestSynopsis() {
+        return testSynopsis;
+    }
+
+    public void setTestSynopsis(String testSynopsis) {
+        this.testSynopsis = testSynopsis;
+    }
+
+    public List<EvaluationBO> getEvaluations() {
+        return evaluations;
+    }
+
+    public void setEvaluations(List<EvaluationBO> evaluations) {
+        this.evaluations = evaluations;
+    }
+
+    public List<QuestionBO> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<QuestionBO> questions) {
+        this.questions = questions;
+    }
+
+    public ThemeBO getTheme() {
+        return theme;
+    }
+
+    public void setTheme(ThemeBO theme) {
+        this.theme = theme;
+    }
+
+    public List<TestResultBO> getTestResults() {
+        return testResults;
+    }
+
+    public void setTestResults(List<TestResultBO> testResults) {
+        this.testResults = testResults;
     }
 
     public String getTestLabel() {
