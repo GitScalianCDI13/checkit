58569fc4548d13e3acc3be2ff3e848454810c0c2
fred <fremaitre@gmail.com>
2017-07-06 11:57:14 +0200

    Mapping

--- business/src/main/java/com/scalian/checkit/service/mapping/ThemeMapping.java
+++ business/src/main/java/com/scalian/checkit/service/mapping/ThemeMapping.java
@@ -1,7 +1,14 @@
 package com.scalian.checkit.service.mapping;
 
+import com.scalian.checkit.model.QuestionEntity;
+import com.scalian.checkit.model.TestEntity;
 import com.scalian.checkit.model.ThemeEntity;
+import com.scalian.checkit.service.model.QuestionBO;
+import com.scalian.checkit.service.model.TestBO;
 import com.scalian.checkit.service.model.ThemeBO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ThemeMapping {
 
@@ -9,20 +16,51 @@ public class ThemeMapping {
         ThemeBO themeBO = new ThemeBO();
         themeBO.setThemeId(themeEntity.getThemeId());
         themeBO.setThemeLabel(themeEntity.getThemeLabel());
-        //List<QuestionBO> questions;
-        //List<TestBO> tests;
 
 
+        // Map des QuestionEntityToBO
+        List<QuestionBO> questionBOList = new ArrayList<>();
+        for (QuestionEntity questionEntity : themeEntity.getQuestions()){
+            QuestionBO questionBO = QuestionMapping.mapQuestionEntityToBO(questionEntity);
+            questionBOList.add(questionBO);
+        }
+        themeBO.setQuestions(questionBOList);
+
+
+        // Map des TestToBO
+        List<TestBO> testBOList = new ArrayList<>();
+        for (TestEntity testEntity : themeEntity.getTests()){
+            TestBO testBO = TestMapping.mapTestEntityToBO(testEntity);
+            testBOList.add(testBO);
+        }
+        themeBO.setTests(testBOList);
 
         return themeBO;
+
     }
 
     public static ThemeEntity mapThemeBOToEntity(ThemeBO themeBO){
         ThemeEntity themeEntity = new ThemeEntity();
         themeEntity.setThemeId(themeBO.getThemeId());
         themeEntity.setThemeLabel(themeBO.getThemeLabel());
-        //List<QuestionBO> questions;
-        //List<TestBO> tests;
+
+        // Map des QuestionBOToEntity
+        List<QuestionEntity> questionEntityList = new ArrayList<>();
+        for (QuestionBO questionBO : themeBO.getQuestions()){
+            QuestionEntity questionEntity = QuestionMapping.mapQuestionBOToEntity(questionBO);
+            questionEntityList.add(questionEntity);
+        }
+        themeEntity.setQuestions(questionEntityList);
+
+
+        // Map des TestBOToEntity
+        List<TestEntity> testEntityList = new ArrayList<>();
+        for (TestBO testBO : themeBO.getTests()){
+            TestEntity testEntity = TestMapping.mapTestBOToEntity(testBO);
+            testEntityList.add(testEntity);
+        }
+        themeEntity.setTests(testEntityList);
+
 
         return themeEntity;
     }
--- business/src/main/java/com/scalian/checkit/service/mapping/UserResponseMapping.java
+++ business/src/main/java/com/scalian/checkit/service/mapping/UserResponseMapping.java
@@ -1,16 +1,56 @@
 package com.scalian.checkit.service.mapping;
 
-import com.scalian.checkit.model.UserResponseEntity;
-import com.scalian.checkit.service.model.UserResponseBO;
+import com.scalian.checkit.model.*;
+import com.scalian.checkit.service.model.*;
 
 public class UserResponseMapping {
 
-    public static UserResponseBO mapUserResponseEntityToBO(UserResponseEntity UserResponseEntity){
+    public static UserResponseBO mapUserResponseEntityToBO(UserResponseEntity userResponseEntity){
         UserResponseBO userResponseBO = new UserResponseBO();
+
+        userResponseBO.setUserResponseId(userResponseEntity.getUserResponseId());
+
+        // Map PossibleResponse
+        PossibleResponseBO possibleResponseBO = PossibleResponseMapping.mapPossibleResponseEntityToBO(userResponseEntity.getPossibleResponse());
+        userResponseBO.setPossibleResponse(possibleResponseBO);
+
+        // Map Question
+        QuestionBO questionBO = QuestionMapping.mapQuestionEntityToBO(userResponseEntity.getQuestion());
+        userResponseBO.setQuestion(questionBO);
+
+        // Map TestResult
+        TestResultBO testResultBO = TestResultMapping.mapTestResultEntityToBO(userResponseEntity.getTestResult());
+        userResponseBO.setTestResult(testResultBO);
+
+        // Map User
+        UserBO userBO = UserMapping.mapUserEntityToBO(userResponseEntity.getUser());
+        userResponseBO.setUser(userBO);
+
         return userResponseBO;
+
     }
     public static UserResponseEntity mapUserResponseBOToEntity(UserResponseBO userResponseBO){
         UserResponseEntity userResponseEntity = new UserResponseEntity();
+
+        userResponseEntity.setUserResponseId(userResponseBO.getUserResponseId());
+
+        // Map PossibleResponse
+        PossibleResponseEntity possibleResponseEntity = PossibleResponseMapping.mapPossibleResponseBOToEntity(userResponseBO.getPossibleResponse());
+        userResponseEntity.setPossibleResponse(possibleResponseEntity);
+
+        // Map Question
+        QuestionEntity questionEntity = QuestionMapping.mapQuestionBOToEntity(userResponseBO.getQuestion());
+        userResponseEntity.setQuestion(questionEntity);
+
+        // Map TestResult
+        TestResultEntity testResultEntity = TestResultMapping.mapTestResultBOToEntity(userResponseBO.getTestResult());
+        userResponseEntity.setTestResult(testResultEntity);
+
+        // Map User
+        UserEntity userEntity = UserMapping.mapUserBOToEntity(userResponseBO.getUser());
+        userResponseEntity.setUser(userEntity);
+
+
         return userResponseEntity;
     }
 }
