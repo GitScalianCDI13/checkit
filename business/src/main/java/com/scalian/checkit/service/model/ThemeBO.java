1e2e6c6c02a66b9edcba53059b34094366e417df
Jérôme Lamy <jerome.lamy@scalian.com>
2017-07-06 09:37:39 +0200

    BO classes

--- business/src/main/java/com/scalian/checkit/service/model/ThemeBO.java
+++ business/src/main/java/com/scalian/checkit/service/model/ThemeBO.java
@@ -1,6 +1,43 @@
 package com.scalian.checkit.service.model;
 
+import java.util.List;
+
 public class ThemeBO {
 
+    private Integer themeId;
+    private String themeLabel;
+    private List<QuestionBO> questions;
+    private List<TestBO> tests;
 
+    public Integer getThemeId() {
+        return themeId;
+    }
+
+    public void setThemeId(Integer themeId) {
+        this.themeId = themeId;
+    }
+
+    public String getThemeLabel() {
+        return themeLabel;
+    }
+
+    public void setThemeLabel(String themeLabel) {
+        this.themeLabel = themeLabel;
+    }
+
+    public List<QuestionBO> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<QuestionBO> questions) {
+        this.questions = questions;
+    }
+
+    public List<TestBO> getTests() {
+        return tests;
+    }
+
+    public void setTests(List<TestBO> tests) {
+        this.tests = tests;
+    }
 }
--- business/src/main/java/com/scalian/checkit/service/model/UserResponseBO.java
+++ business/src/main/java/com/scalian/checkit/service/model/UserResponseBO.java
@@ -2,5 +2,49 @@ package com.scalian.checkit.service.model;
 
 public class UserResponseBO {
 
+    private Integer userResponseId;
+    private PossibleResponseBO possibleResponse;
+    private QuestionBO question;
+    private TestResultBO testResult;
+    private UserBO user;
 
+    public Integer getUserResponseId() {
+        return userResponseId;
+    }
+
+    public void setUserResponseId(Integer userResponseId) {
+        this.userResponseId = userResponseId;
+    }
+
+    public PossibleResponseBO getPossibleResponse() {
+        return possibleResponse;
+    }
+
+    public void setPossibleResponse(PossibleResponseBO possibleResponse) {
+        this.possibleResponse = possibleResponse;
+    }
+
+    public QuestionBO getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(QuestionBO question) {
+        this.question = question;
+    }
+
+    public TestResultBO getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(TestResultBO testResult) {
+        this.testResult = testResult;
+    }
+
+    public UserBO getUser() {
+        return user;
+    }
+
+    public void setUser(UserBO user) {
+        this.user = user;
+    }
 }
